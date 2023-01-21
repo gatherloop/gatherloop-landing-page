dbb0f020cf2a8bc182ef3a637b5726fd0f7e0204
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2023-01-21 20:55:10 +0700

    feat: add join path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/join",
+        destination: "https://chat.whatsapp.com/Fhd16OBC7HtEMSdPUmJR9k",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
