e9c92d094b84c8bd05b4e5db0976ebca74862eef
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2023-03-05 07:09:02 +0700

    feat: testing deployment

--- src/presentations/organisms/navbar/Navbar.tsx
+++ src/presentations/organisms/navbar/Navbar.tsx
@@ -34,6 +34,7 @@ export function Navbar(_props: NavbarProps) {
       <Container flexDirection="row" alignItems="center">
         <YStack flex={1}>
           <Logo width={165} height={35} />
+          <Paragraph>Hehe</Paragraph>
         </YStack>
         <XStack space="$5" display="none" $gtXs={{ display: "flex" }}>
           {Object.entries(menuItems).map(([_key, { title, id }]) => (
