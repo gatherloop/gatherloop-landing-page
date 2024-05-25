a59afa4e6b769e19f9dfcdc77b1d4c095409bc80
M Nindra Zaka <mnindrazaka@gmail.com>
2024-05-26 05:33:40 +0700

    feat: redirect / to gatherloop.cafe instagram

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,13 @@
 import { HomeScreen } from "@/screens";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "https://www.instagram.com/gatherloop.cafe/",
+      permanent: false,
+    },
+  };
+};
+
 export default HomeScreen;
